96126cd3170622f3d9fa886ca033ab12b87cb562
ilkrbk <ilkrbk@gmail.com>
2020-03-17 10:26:57 +0200

    first commit

---
+++ CombSort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CombSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
